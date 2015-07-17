ce39139f712c33e9ff0e774e0a30c0f9a7682d64
Thedischman <milesthedisch@gmail.com>
2015-07-17 18:01:13 +1000

    js file option

--- app/index.js
+++ app/index.js
@@ -1,32 +1,48 @@
-var componentName;
 var generators = require('yeoman-generator');
 var fs = require('fs');
 
 var StylguideComponent = module.exports = generators.Base.extend({
 
-    prompting: function () {
+    prompting: function() {
         var done = this.async();
         this.prompt({
             type    : 'input',
             name    : 'name',
             message : 'Your component name',
             default : this.appname // Default to current folder name
-        }, function (answers) {
-            this.log(componentName = answers.name);
+        },
+        function (answers) {
+            makeComponent(this, answers.name);
             done();
         }.bind(this));
     },
 
-    createComponent: function () {
-        this.mkdir(componentName);
-        this.write(componentName + '/_' + componentName + '.scss' ,
-            '// ' + componentName.charAt(0).toUpperCase() + componentName.slice(1) + '\n'+
-            '//\n' +
-            '// Markup: ' + componentName + '.html\n' +
-            '//\n' +
-            '// Styleguide components.' + componentName
-        );
-        this.write(componentName + '/' + componentName + '.html', "");
+    prompting2: function() {
+        var done = this.async();
+        this.prompt({
+            type    : 'confirm',
+            name    : 'name',
+            message : 'Do you have a jsFile?',
+            default : false
+        },
+        function (answers) {
+             if(answers.name == true) {
+                this.write(answers.name + '/' + answers.name + '.js', "");
+                done();
+             }
+        }.bind(this));
     }
 
 });
+
+function makeComponent(yo, name) {
+    yo.mkdir(name);
+    yo.write(name + '/_' + name + '.scss' ,
+        '// ' + name.charAt(0).toUpperCase() + name.slice(1) + '\n'+
+        '//\n' +
+        '// Markup: ' + name + '.html\n' +
+        '//\n' +
+        '// Styleguide components.' + name
+    );
+    yo.write(name + '/' + name + '.html', "");
+}
